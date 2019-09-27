ed283081aab7a89ba43dcbffb86e3e6c13052652
Simen Bekkhus <sbekkhus91@gmail.com>
2019-09-27 16:35:25 +0200

    fix(ts): add `enableInExpoDevelopment` to typings (#75)

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,8 @@
-export * from "@sentry/react-native";
+import { ReactNativeOptions } from '@sentry/react-native';
+export * from '@sentry/react-native';
+
+export interface ExpoOptions extends ReactNativeOptions {
+  enableInExpoDevelopment?: boolean;
+}
+
+export function init(options: ExpoOptions): void;

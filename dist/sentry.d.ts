e9d36e9180f646720cdb71f9024cffa07a5d2cf2
Cedric van Putten <me@bycedric.com>
2020-07-13 17:44:19 +0200

    fix: restructure to fix managed support (#124)

---
+++ dist/sentry.d.ts
@@ -0,0 +1,2 @@
+import { SentryExpoNativeOptions, SentryExpoWebOptions } from './utils';
+export declare const init: (options?: SentryExpoNativeOptions | SentryExpoWebOptions) => void;
---
+++ src/utils.ts
@@ -0,0 +1,24 @@
+import { BrowserOptions } from '@sentry/browser';
+import { ReactNativeOptions } from '@sentry/react-native';
+import { Integration } from '@sentry/types';
+
+export interface SentryExpoWebOptions extends BrowserOptions {
+  enableInExpoDevelopment?: boolean;
+}
+
+export interface SentryExpoNativeOptions extends ReactNativeOptions {
+  enableInExpoDevelopment?: boolean;
+}
+
+export function overrideDefaultIntegrations(defaults: Integration[], overrides: Integration[]): Integration[] {
+  const overrideIntegrationNames: string[] = overrides.map((each) => each.name);
+  const result: Integration[] = [];
+
+  defaults.forEach((each) => {
+    if (!overrideIntegrationNames.includes(each.name)) {
+      result.push(each);
+    }
+  });
+
+  return [...result, ...overrides];
+}

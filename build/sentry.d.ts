ebb18d229f36fa8d16846f5c7efcd484f002f3c5
Kim Brandwijk <kim.brandwijk@gmail.com>
2022-08-05 02:17:40 +0200

    bump expo-module-scripts to fix dependency version conflicts

--- build/sentry.d.ts
+++ build/sentry.d.ts
@@ -1,3 +1,4 @@
 import { SentryExpoNativeOptions } from './utils';
 export * as Native from '@sentry/react-native';
 export declare const init: (options?: SentryExpoNativeOptions) => void;
+//# sourceMappingURL=sentry.d.ts.map

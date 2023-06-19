c66748ffec25061a2e1de7c09ac026ce881039fd
kbrandwijk <kim.brandwijk@gmail.com>
2023-06-19 13:03:07 +0200

    v6.2.2

--- build/version.js
+++ build/version.js
@@ -4,7 +4,7 @@ exports.SENTRY_REACT_VERSION = exports.SENTRY_REACT_PACKAGE = exports.SENTRY_REA
 const react_1 = require("@sentry/react");
 Object.defineProperty(exports, "SENTRY_REACT_VERSION", { enumerable: true, get: function () { return react_1.SDK_VERSION; } });
 exports.SENTRY_EXPO_PACKAGE = 'npm:sentry-expo';
-exports.SENTRY_EXPO_VERSION = '6.2.1';
+exports.SENTRY_EXPO_VERSION = '6.2.2';
 exports.SENTRY_REACT_NATIVE_PACKAGE = 'npm:@sentry/react-native';
 exports.SENTRY_REACT_PACKAGE = 'npm:@sentry/react';
 //# sourceMappingURL=version.js.map
--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,7 @@
 import { SDK_VERSION as SENTRY_REACT_VERSION } from '@sentry/react';
 
 export const SENTRY_EXPO_PACKAGE = 'npm:sentry-expo';
-export const SENTRY_EXPO_VERSION = '6.2.1';
+export const SENTRY_EXPO_VERSION = '6.2.2';
 
 export const SENTRY_REACT_NATIVE_PACKAGE = 'npm:@sentry/react-native';
 export const SENTRY_REACT_PACKAGE = 'npm:@sentry/react';

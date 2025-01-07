ef81a3ccc9ff6de9de734c696a995d2ed7237726
Nick Novitski <github@nicknovitski.com>
2025-01-07 11:41:27 -0800

    fix: update build

--- build/version.d.ts
+++ build/version.d.ts
@@ -1,6 +1,6 @@
 import { SDK_VERSION as SENTRY_REACT_VERSION } from '@sentry/react';
 export declare const SENTRY_EXPO_PACKAGE = "npm:sentry-expo";
-export declare const SENTRY_EXPO_VERSION = "7.1.1";
+export declare const SENTRY_EXPO_VERSION = "7.2.0";
 export declare const SENTRY_REACT_NATIVE_PACKAGE = "npm:@sentry/react-native";
 export declare const SENTRY_REACT_PACKAGE = "npm:@sentry/react";
 export { SENTRY_REACT_VERSION };
